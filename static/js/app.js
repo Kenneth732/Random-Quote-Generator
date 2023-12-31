62f7022d2c12bdc669c0f01ac7151b5ac77da961
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-12-31 18:07:22 +0300

    first commit

---
+++ static/js/app.js
@@ -0,0 +1,48 @@
+const quoteText = document.querySelector('#quoteText')
+const newQuoteBtn = document.querySelector('#newQuoteBtn')
+
+const backgroundImageUrls = [
+  "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/122e2813-42b0-4ada-8d0a-20631b711365/dg2kgr9-424a1503-0544-4ee9-920e-cfefe57ec243.jpg/v1/fill/w_894,h_894,q_70,strp/dark_fantasy_by_m1ndm4k3rs_dg2kgr9-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTI4MCIsInBhdGgiOiJcL2ZcLzEyMmUyODEzLTQyYjAtNGFkYS04ZDBhLTIwNjMxYjcxMTM2NVwvZGcya2dyOS00MjRhMTUwMy0wNTQ0LTRlZTktOTIwZS1jZmVmZTU3ZWMyNDMuanBnIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.ToDEgJJ2qeXNw-CJL8Ha2ueZDnhpdQQrZyv_NdikgH0",
+
+  "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/122e2813-42b0-4ada-8d0a-20631b711365/dg222cg-5036c58f-05be-43a1-8703-51a3b1accd13.jpg/v1/fill/w_894,h_894,q_70,strp/let_it_rain_by_m1ndm4k3rs_dg222cg-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTAyNCIsInBhdGgiOiJcL2ZcLzEyMmUyODEzLTQyYjAtNGFkYS04ZDBhLTIwNjMxYjcxMTM2NVwvZGcyMjJjZy01MDM2YzU4Zi0wNWJlLTQzYTEtODcwMy01MWEzYjFhY2NkMTMuanBnIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.xs4vyh6cdVFtKfvtTtnKPt9nX88GP16VRRcArW11XWQ",
+
+  "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/122e2813-42b0-4ada-8d0a-20631b711365/dg35t8g-da2d00ec-167a-479e-ac26-1ce4243bdafc.png/v1/fill/w_894,h_894,q_70,strp/the_girl_with_the_tattoo_by_m1ndm4k3rs_dg35t8g-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTAyNCIsInBhdGgiOiJcL2ZcLzEyMmUyODEzLTQyYjAtNGFkYS04ZDBhLTIwNjMxYjcxMTM2NVwvZGczNXQ4Zy1kYTJkMDBlYy0xNjdhLTQ3OWUtYWMyNi0xY2U0MjQzYmRhZmMucG5nIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.4-u4GJdvdKf-13XzODo7VTU7FqyvW2qhwSB6W9YZXac",
+
+  "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d246cca6-17e0-4e8a-8725-0ac0a76c1e44/dg37gj9-228d6a9e-1dc5-4a92-b886-1e9b900d4a99.png/v1/fill/w_894,h_894,q_70,strp/in_new_zealand_for_the_world_cup__by_namiacimora_dg37gj9-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTAyNCIsInBhdGgiOiJcL2ZcL2QyNDZjY2E2LTE3ZTAtNGU4YS04NzI1LTBhYzBhNzZjMWU0NFwvZGczN2dqOS0yMjhkNmE5ZS0xZGM1LTRhOTItYjg4Ni0xZTliOTAwZDRhOTkucG5nIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.sPlg_KIkfn5H4saOK1myIaBicNDfIK6VnRCpDOo3XKY",
+
+  "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/463adb00-62d0-49dc-8a5c-0d68b68d601f/dg37cm2-8c52fa34-e92a-4b2e-89c5-62e32e31d451.png/v1/fit/w_512,h_768,q_70,strp/1girl_black_shirts_by_battlemasterleo_dg37cm2-375w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzY4IiwicGF0aCI6IlwvZlwvNDYzYWRiMDAtNjJkMC00OWRjLThhNWMtMGQ2OGI2OGQ2MDFmXC9kZzM3Y20yLThjNTJmYTM0LWU5MmEtNGIyZS04OWM1LTYyZTMyZTMxZDQ1MS5wbmciLCJ3aWR0aCI6Ijw9NTEyIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.wq98r6vfkBgIa3LjiHlG3vxE92qEsvfFxCC6Smw37xw",
+
+  "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/0d4025a9-220f-489d-8d1f-9b5f8e849636/dg3541q-5cc241fa-e80d-4d59-9cd9-cba892a8b3ce.jpg/v1/fill/w_1195,h_669,q_70,strp/what_else_is_there__by_danielsleepwalker_dg3541q-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzE3IiwicGF0aCI6IlwvZlwvMGQ0MDI1YTktMjIwZi00ODlkLThkMWYtOWI1ZjhlODQ5NjM2XC9kZzM1NDFxLTVjYzI0MWZhLWU4MGQtNGQ1OS05Y2Q5LWNiYTg5MmE4YjNjZS5qcGciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.6NMYRbyJdjNmGQr6kvq-S-NNGmGFkLjsADW9yyLevUw",
+];
+
+
+function getRandomBackgroundImageUrl(){
+  const randomIndex = Math.floor(Math.random() * backgroundImageUrls.length)
+  return backgroundImageUrls[randomIndex]
+}
+
+function fetchRandomQuote(){
+  return fetch('')
+  .then((res) => {
+    if(res.ok) {
+      throw new Error('Network resp was not ok')
+    }
+    return res.json();
+  })
+  .then((data) => data.content)
+  .catch(error => {
+    console.error("Error fetching quote: ", error)
+    return "Error: Failed to Fetch Quotes"
+  });
+}
+
+function updateQuote() {
+  const backgroundImageUrl = getRandomBackgroundImageUrl();
+  document.body.style.backgroundImage = `url(${backgroundImageUrl})`
+
+  fetchRandomQuote().then(quote => {
+    quoteText.textContent = quote;
+  })
+}
+
+newQuoteBtn.addEventListener
--- static/js/script.js
+++ static/js/script.js
@@ -1,123 +1,115 @@
-// script.js
-const quoteText = document.getElementById("quoteText");
-const newQuoteBtn = document.getElementById("newQuoteBtn");
+// class QuoteApp {
+//   constructor() {
+//       this.quoteText = document.getElementById("quoteText");
+//       this.newQuoteBtn = document.getElementById("newQuoteBtn");
+//       this.backgroundImageUrls = [
+//           "./assetsp1.jpg",
+//           "./assetsp2.jpg",
+//           "./assetsp3.jpg",
+//           "./assetsp4.jpg",
+//           "./assetsp5.jpg",
+//           "./assetsp6.jpg",
+//           "./assetsp7.jpg",
+//           "./assetsp8.jpg",
+//           "./assetsp9.jpg",
+//           "./assetsp10.jpg",
+//       ];
+//       this.currentBackgroundImageIndex = 0;
 
-const backgroundImageUrls = [
-  "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/122e2813-42b0-4ada-8d0a-20631b711365/dg2kgr9-424a1503-0544-4ee9-920e-cfefe57ec243.jpg/v1/fill/w_894,h_894,q_70,strp/dark_fantasy_by_m1ndm4k3rs_dg2kgr9-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTI4MCIsInBhdGgiOiJcL2ZcLzEyMmUyODEzLTQyYjAtNGFkYS04ZDBhLTIwNjMxYjcxMTM2NVwvZGcya2dyOS00MjRhMTUwMy0wNTQ0LTRlZTktOTIwZS1jZmVmZTU3ZWMyNDMuanBnIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.ToDEgJJ2qeXNw-CJL8Ha2ueZDnhpdQQrZyv_NdikgH0",
+//       this.newQuoteBtn.addEventListener("click", () => {
+//           this.fetchRandomQuote();
+//           this.changeBackgroundImage();
+//       });
+//   }
 
-  "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/122e2813-42b0-4ada-8d0a-20631b711365/dg222cg-5036c58f-05be-43a1-8703-51a3b1accd13.jpg/v1/fill/w_894,h_894,q_70,strp/let_it_rain_by_m1ndm4k3rs_dg222cg-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTAyNCIsInBhdGgiOiJcL2ZcLzEyMmUyODEzLTQyYjAtNGFkYS04ZDBhLTIwNjMxYjcxMTM2NVwvZGcyMjJjZy01MDM2YzU4Zi0wNWJlLTQzYTEtODcwMy01MWEzYjFhY2NkMTMuanBnIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.xs4vyh6cdVFtKfvtTtnKPt9nX88GP16VRRcArW11XWQ",
-
-  "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/122e2813-42b0-4ada-8d0a-20631b711365/dg35t8g-da2d00ec-167a-479e-ac26-1ce4243bdafc.png/v1/fill/w_894,h_894,q_70,strp/the_girl_with_the_tattoo_by_m1ndm4k3rs_dg35t8g-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTAyNCIsInBhdGgiOiJcL2ZcLzEyMmUyODEzLTQyYjAtNGFkYS04ZDBhLTIwNjMxYjcxMTM2NVwvZGczNXQ4Zy1kYTJkMDBlYy0xNjdhLTQ3OWUtYWMyNi0xY2U0MjQzYmRhZmMucG5nIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.4-u4GJdvdKf-13XzODo7VTU7FqyvW2qhwSB6W9YZXac",
-
-  "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d246cca6-17e0-4e8a-8725-0ac0a76c1e44/dg37gj9-228d6a9e-1dc5-4a92-b886-1e9b900d4a99.png/v1/fill/w_894,h_894,q_70,strp/in_new_zealand_for_the_world_cup__by_namiacimora_dg37gj9-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTAyNCIsInBhdGgiOiJcL2ZcL2QyNDZjY2E2LTE3ZTAtNGU4YS04NzI1LTBhYzBhNzZjMWU0NFwvZGczN2dqOS0yMjhkNmE5ZS0xZGM1LTRhOTItYjg4Ni0xZTliOTAwZDRhOTkucG5nIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.sPlg_KIkfn5H4saOK1myIaBicNDfIK6VnRCpDOo3XKY",
-
-  "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/463adb00-62d0-49dc-8a5c-0d68b68d601f/dg37cm2-8c52fa34-e92a-4b2e-89c5-62e32e31d451.png/v1/fit/w_512,h_768,q_70,strp/1girl_black_shirts_by_battlemasterleo_dg37cm2-375w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzY4IiwicGF0aCI6IlwvZlwvNDYzYWRiMDAtNjJkMC00OWRjLThhNWMtMGQ2OGI2OGQ2MDFmXC9kZzM3Y20yLThjNTJmYTM0LWU5MmEtNGIyZS04OWM1LTYyZTMyZTMxZDQ1MS5wbmciLCJ3aWR0aCI6Ijw9NTEyIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.wq98r6vfkBgIa3LjiHlG3vxE92qEsvfFxCC6Smw37xw",
-
-  "",
-  // Add more image URLs as needed
-];
-
-function getRandomBackgroundImageUrl() {
-  const randomIndex = Math.floor(Math.random() * backgroundImageUrls.length);
-  return backgroundImageUrls[randomIndex];
-}
-
-// Function to fetch a random quote from an API using Promises
-function fetchRandomQuote() {
-  return fetch("https://api.quotable.io/random")
-    .then(response => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
-    })
-    .then(data => data.content)
-    .catch(error => {
-      console.error("Error fetching quote:", error);
-      return "Error: Failed to fetch quote";
-    });
-}
-
-// Function to update the displayed quote and background image
-function updateQuote() {
-  const backgroundImageUrl = getRandomBackgroundImageUrl();
-  document.body.style.backgroundImage = `url(${backgroundImageUrl})`;
-
-  fetchRandomQuote().then(quote => {
-    quoteText.textContent = quote;
-  });
-}
-
-// Event listener for the "New Quote" button
-newQuoteBtn.addEventListener("click", updateQuote);
-
-// Initial quote update on page load
-updateQuote();
-
-
-// const quoteText = document.getElementById("quoteText");
-// const newQuoteBtn = document.getElementById("newQuoteBtn");
-
-// // Function to fetch a random quote from an API using Promises
-// function fetchRandomQuote() {
-//   return fetch("https://api.quotable.io/random")
-//     .then(response => {
-//       if (!response.ok) {
-//         throw new Error("Network response was not ok");
+//   async fetchRandomQuote() {
+//       try {
+//           const response = await fetch("https://api.quotable.io/random");
+//           const data = await response.json();
+//           const quote = data.content;
+//           this.displayQuote(quote);
+//       } catch (error) {
+//           console.error("Error fetching a new quote:", error);
 //       }
-//       return response.json();
-//     })
-//     .then(data => data.content)
-//     .catch(error => {
-//       console.error("Error fetching quote:", error);
-//       return "Error: Failed to fetch quote";
-//     });
-// }
+//   }
 
-// // Function to update the displayed quote
-// function updateQuote() {
-//   fetchRandomQuote().then(quote => {
-//     quoteText.textContent = quote;
-//   });
-// }
+//   displayQuote(quote) {
+//       this.quoteText.textContent = quote;
+//   }
 
-// // Event listener for the "New Quote" button
-// newQuoteBtn.addEventListener("click", updateQuote);
+//   changeBackgroundImage() {
+//       // Get the container element to change the background
+//       const container = document.querySelector(".container");
 
-// // Initial quote update on page load
-// updateQuote();
+//       // Increment the current background image index and wrap around if needed
+//       this.currentBackgroundImageIndex = (this.currentBackgroundImageIndex + 1) % this.backgroundImageUrls.length;
 
-
-// another
-// another
-// another
-
-// const quoteText = document.getElementById("quoteText");
-// const newQuoteBtn = document.getElementById("newQuoteBtn");
-
-// // Function to fetch a random quote from an API
-// async function fetchRandomQuote() {
-//   try {
-//     const response = await fetch("https://api.quotable.io/random");
-//     if (!response.ok) {
-//       throw new Error("Network response was not ok");
-//     }
-//     const data = await response.json();
-//     return data.content;
-//   } catch (error) {
-//     console.error("Error fetching quote:", error);
-//     return "Error: Failed to fetch quote";
+//       // Set the background image of the container
+//       container.style.backgroundImage = `url(${this.backgroundImageUrls[this.currentBackgroundImageIndex]})`;
 //   }
 // }
 
-// // Function to update the displayed quote
-// async function updateQuote() {
-//   const quote = await fetchRandomQuote();
-//   quoteText.textContent = quote;
-// }
+// // Initialize the QuoteApp
+// const quoteApp = new QuoteApp();
+// quoteApp.fetchRandomQuote(); // Fetch and display an initial quote
+// quoteApp.changeBackgroundImage(); // Set an initial background image
 
-// // Event listener for the "New Quote" button
-// newQuoteBtn.addEventListener("click", updateQuote);
 
-// // Initial quote update on page load
-// updateQuote();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class QuoteApp {
+  constructor() {
+      this.quoteText = document.getElementById("quoteText");
+      this.newQuoteBtn = document.getElementById("newQuoteBtn");
+
+      this.newQuoteBtn.addEventListener("click", this.fetchRandomQuote.bind(this));
+  }
+
+  async fetchRandomQuote() {
+      try {
+          const response = await fetch("https://api.quotable.io/random");
+          const data = await response.json();
+          const quote = data.content;
+          this.displayQuote(quote);
+      } catch (error) {
+          console.error("Error fetching a new quote:", error);
+      }
+  }
+
+  displayQuote(quote) {
+      this.quoteText.textContent = quote;
+  }
+}
+
+// Initialize the QuoteApp
+const quoteApp = new QuoteApp();
+quoteApp.fetchRandomQuote(); // Fetch and display an initial quote
